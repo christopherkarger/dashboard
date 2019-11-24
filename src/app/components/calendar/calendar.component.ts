a09a17e482f7c793f4c9c352c9436e552d5d86f2
Christopher Karger <karger1986@gmail.com>
2019-11-24 20:30:12 +0100

    styled a bit

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -14,6 +14,7 @@ export class CalendarComponent implements OnInit, AfterViewInit {
       useCreationPopup: true,
       defaultView: "day",
       usageStatistics: false,
+      taskView: false,
       template: {
         timegridDisplayPrimaryTime: time => {
           return `${time.hour < 10 ? 0 : ""}${time.hour}:${
@@ -21,29 +22,31 @@ export class CalendarComponent implements OnInit, AfterViewInit {
           }${time.minutes}`;
         }
 
+      },
+      theme: {
       }
     });
 
-    calendar.createSchedules([
-      {
-        id: "1",
-        calendarId: "1",
-        title: "my schedule",
-        category: "time",
-        dueDateClass: "",
-        start: "2019-11-22T22:30:00+09:00",
-        end: "2019-11-22T02:30:00+09:00"
-      },
-      {
-        id: "2",
-        calendarId: "1",
-        title: "second schedule",
-        category: "time",
-        dueDateClass: "",
-        start: "2019-11-22T17:30:00+09:00",
-        end: "2019-11-22T17:31:00+09:00"
-      }
-    ]);
+    // calendar.createSchedules([
+    //   {
+    //     id: "1",
+    //     calendarId: "1",
+    //     title: "my schedule",
+    //     category: "time",
+    //     dueDateClass: "",
+    //     start: "2019-11-22T22:30:00+09:00",
+    //     end: "2019-11-22T02:30:00+09:00"
+    //   },
+    //   {
+    //     id: "2",
+    //     calendarId: "1",
+    //     title: "second schedule",
+    //     category: "time",
+    //     dueDateClass: "",
+    //     start: "2019-11-22T17:30:00+09:00",
+    //     end: "2019-11-22T17:31:00+09:00"
+    //   }
+    // ]);
 
     calendar.on("beforeCreateSchedule", function(event) {
       // calendar.createSchedules(event);
